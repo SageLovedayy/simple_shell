dc42b1e51f498c3af695fa2641b46216164d8449
Isaac <isaacomenuche@gmail.com>
2023-11-09 21:51:34 +0100

    cd argument implemented

--- built-in.c
+++ built-in.c
@@ -48,16 +48,101 @@ void executeLS(int argc, char *argv[])
 	}
 }
 
+/******************** Still buggy *******************/
 /**
- * executeCD - Execute command that change directory
- * @argc: Argument count
- * @argv: Argument vector
- * Return: void
+ * executeCD - Execute the 'cd' built-in command
+ * @argc: argument count
+ * @argv: argument vector
  */
 void executeCD(int argc, char *argv[])
 {
-	(void)argc;
-	(void)argv;
+	char current_directory[BUF_SIZE];
+
+	if (argc > 2)
+	{
+		print_str("cd: too many arguments\n");
+		return;
+	}
+
+	if (argc == 1 || (argc == 2 && _strcmp(argv[1], "~") == 0))
+	{
+		/* No argument provided or '~' given, change to the home directory */
+		if (chdir(getenv("HOME")) == 0)
+		{
+			/* Update the PWD environment variable */
+			if (getcwd(current_directory, sizeof(current_directory)) != NULL)
+			{
+				setenv("PWD", current_directory, 1);
+			}
+			else
+			{
+				perror("cd");
+			}
+		}
+		else
+		{
+			perror("cd");
+		}
+	}
+	else if (argc == 2)
+	{
+		/* Change to the specified directory */
+		if (_strcmp(argv[1], "-") == 0)
+		{
+			/* Handle 'cd -': change to the previous directory */
+			char *previous_dir = getenv("OLDPWD");
+			if (previous_dir != NULL)
+			{
+				if (chdir(previous_dir) == 0)
+				{
+					/* Update PWD and OLDPWD environment variables */
+					if (getcwd(current_directory, sizeof(current_directory)) != NULL)
+					{
+						setenv("PWD", current_directory, 1);
+						setenv("OLDPWD", argv[1], 1);
+					}
+					else
+					{
+						perror("cd");
+					}
+				}
+				else
+				{
+					perror("cd");
+				}
+			}
+			else
+			{
+				print_str("cd: OLDPWD not set\n");
+			}
+		}
+		else
+		{
+			/* Change to the specified directory */
+			if (chdir(argv[1]) == 0)
+			{
+				/* Update the PWD and OLDPWD environment variables */
+				if (getcwd(current_directory, sizeof(current_directory)) != NULL)
+				{
+					setenv("PWD", current_directory, 1);
+					setenv("OLDPWD", argv[1], 1);
+				}
+				else
+				{
+					perror("cd");
+				}
+			}
+			else
+			{
+				perror("cd");
+			}
+		}
+	}
+	else
+	{
+		/* Invalid usage of 'cd' command */
+		printf("cd: too many arguments\n");
+	}
 }
 
 /**
