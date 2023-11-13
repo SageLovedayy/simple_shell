1e96bf1a37041a9d7f22a414b02a5ad2e6c62d53
Loveday Nnabuife <116376154+SageLovedayy@users.noreply.github.com>
2023-11-13 12:50:52 +0100

    Trackback

--- main.h
+++ main.h
@@ -13,22 +13,90 @@
 #include <string.h>
 
 #define BUF_SIZE 1024
+#define FLUSH_BUFFER -1
 
 extern char **environ;
+
+/**
+* struct Node - singly linked list node
+* @index: numerical index/ identifier
+* @value: string value
+* @next: pointer to next node
+*/
+typedef struct Node
+{
+	int index;
+	char *value;
+	struct Node *next;
+} listNode;
+
+/**
+* struct pseudoArgs - struct of pseudo arguments to pass into a function
+* @current_argument: add descr
+* @command_arguments: add descr
+* @executable_path: add descr
+* @argument_count: add descr
+* @current_line_number: add descr
+* @error_number: add descr
+* @line_count_flag: add descr
+* @file_name: add descr
+* @environment_variables: add descr
+* @environment: add descr
+* @command_history: add descr
+* @alias_list: add descr
+* @environment_changed_flag: add descr
+* @commandExecStatus: add descr
+* @command_buffer: add descr
+* @command_buffer_type: add descr
+* @input_file_descriptor: add descr
+* @history_count: add descr
+*/
+typedef struct pseudoArgs
+{
+	char *current_argument;
+	char **command_arguments;
+	char *executable_path;
+	char *file_name;
+	listNode *environment_variables;
+	listNode *command_history;
+	listNode *alias_list;
+	char **environment;
+	char **command_buffer;
+	int argument_count;
+	unsigned int current_line_number;
+	int error_number;
+	int line_count_flag;
+	int environment_changed_flag;
+	int commandExecStatus;
+	int command_buffer_type;
+	int input_file_descriptor;
+	int history_count;
+} commandInfo;
+
+/*Init commandInfo pseudoArg vals to NULL for ptrs, 0 for other data types*/
+#define COMMAND_INFO_INIT\
+{NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, \
+		0, 0, 0, 0, 0, 0, 0, 0, 0}
+
 
 /**
  * struct BuiltInCommand - Represents a built-in shell command
  * @name: The name of the built-in command.
  * @CommandFunction: A function pointer to handle the command.
  */
-struct BuiltInCommand
+typedef struct BuiltInCommand
 {
 	/* Name of the built-in command */
 	const char *name;
 	/* Function pointer for handling the command */
-	void (*CommandFunction)(int argc, char *argv[]);
-};
-typedef struct BuiltInCommand commandStruct;
+	int (*CommandFunction)(commandInfo *);
+} commandStruct;
+
+
+
+
+/*=================================================================*/
+
 
 char *argChecker(int argc, char *cmd);
 bool flagChecker(const char *token);
